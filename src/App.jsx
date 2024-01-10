ee1076fce3386fceff102db0402ef71181e44ca4
rimanshusingh4 <rimanshusingh4@gmail.com>
2024-01-11 01:22:24 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  return (
+    <>
+      <h1>Let's build Mega Blog Project.</h1>
+    </>
+  )
+}
+
+export default App
